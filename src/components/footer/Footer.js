9054ea69a45b6ca9ddb0dd5272865e8619a248b3
safvanavci <43886857+safvanavci@users.noreply.github.com>
2022-11-14 15:55:22 +0300

    Footer
    Footer eklendi

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Theme from "../header/Theme";
+import Social from "./Social";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+
+      <div className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </div>
+
+      <Social />
+
+      <button className="btn btn-ghost btn-circle">
+        <Theme />
+      </button>
+
+      <div>
+        <p>Copyright © 2022 </p>
+      </div>
+      
+    </footer>
+  );
 }
